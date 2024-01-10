012e99d2f721c13270678d4b20afad8cd42ea1ae
Tiago <tiagopassos950@gmail.com>
2024-01-10 12:18:09 -0300

    Foi adicionado a telas maiores o formulário de login.

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,21 @@
-function encontrar-dispatchEvent(pesquisa,palavra-chave){
-    
-}
+
+// seleciona o primeiro input
+var currentSlide = 1;
+document.getElementById('imagem' + currentSlide).checked = true;
+
+// muda de slide a cada 3 segundos
+setInterval(function() {
+  // aumenta o contador de slide
+  currentSlide++;
+
+  // se chegou ao último slide, volta para o primeiro
+  if (currentSlide > 5) {
+    currentSlide = 1;
+  }
+
+  // seleciona o input correspondente ao slide atual
+  document.getElementById('imagem' + currentSlide).checked = true;
+}, 5000);
+
+
+
