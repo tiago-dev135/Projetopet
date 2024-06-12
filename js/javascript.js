ba5f8c28e541f4e0d0299c06bd617f02765ba3ba
Tiago <tiagopassos950@gmail.com>
2024-06-12 20:19:46 -0300

    Atualização de formulário

--- js/javascript.js
+++ js/javascript.js
@@ -31,3 +31,21 @@ function activeDropDown(){
   }
 }
 
+function validateForm() {
+ 
+
+  // Obtém os valores dos campos do formulário
+  const nome = document.querySelector('[name="nome"]').value;
+  const contato = document.querySelector('[name="contato"]').value;
+  const email = document.querySelector('[name="Email"]').value;
+  const senha = document.querySelector('[name="senha"]').value;
+
+  // Verifica se todos os campos foram preenchidos
+  if (nome && contato && email && senha) {
+    // Exibe um alerta de sucesso usando SweetAlert
+    swal("Conta criada com sucesso!", `Bem-vindo, ${nome}!`, "success");
+  } else {
+    // Exibe um alerta de erro caso algum campo não esteja preenchido
+    swal("Erro", "Por favor, preencha todos os campos.", "error");
+  }
+}
