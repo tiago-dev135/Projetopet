5a7db30acbf1fc675eed59b8a09588d8d1d6c502
Tiago <tiagopassos950@gmail.com>
2025-08-12 19:21:53 -0300

    Incluindo nova seção.

--- js/javascript.js
+++ js/javascript.js
@@ -49,3 +49,65 @@ function validateForm() {
     swal("Erro", "Por favor, preencha todos os campos.", "error");
   }
 }
+
+let categoriasVantagens = document.getElementsByClassName("vantagens");
+let imgDescricao = document.querySelector(".descricao img");
+let textoDescricao = document.querySelector(".descricao span");
+
+let dados = [
+  {
+    texto: "Primeira descrição sobre a vantagem 1.",
+    imagem: "https://th.bing.com/th/id/R.9d467dfc221b6ab5d434c054ac5c8c42?rik=gGrec1MNsSY16A&pid=ImgRaw&r=0"
+  },
+  {
+    texto: "Segunda descrição sobre a vantagem 2.",
+    imagem: "https://img.freepik.com/vetores-premium/empresario-sobe-a-escada-para-o-sucesso-escada-do-sucesso-escada-para-ter-sucesso-e-alcancar_105700-320.jpg?w=2000"
+  },
+  {
+    texto: "Terceira descrição sobre a vantagem 3.",
+    imagem: "https://th.bing.com/th/id/R.3a1b0535f29e40315704ece7a34171ac?rik=8RrQNHqb9rUnzg&riu=http%3a%2f%2f4.bp.blogspot.com%2f-l4G9nRQmjH8%2fXb3dhJOUqEI%2fAAAAAAABgJY%2fB5typB9wDuEsrAZZ09UZte8PEjFa1CDWACK4BGAYYCw%2fs1600%2f6f8d7f72-88d6-4ed7-bb88-52d9059ab0fe.jpg&ehk=nn5AEJnMlV8QkSu%2bmxTMnKFigeHfyrYcoXEdOUgNdYk%3d&risl=&pid=ImgRaw&r=0"
+  }
+];
+
+let i = 0;
+let intervalo;
+
+// Função para atualizar a exibição
+function atualizarVantagem(index) {
+  // Remove ativo de todos
+  for (let j = 0; j < categoriasVantagens.length; j++) {
+    categoriasVantagens[j].classList.remove("ativo");
+  }
+
+  // Adiciona ativo ao selecionado
+  categoriasVantagens[index].classList.add("ativo");
+
+  // Atualiza imagem e texto
+  imgDescricao.src = dados[index].imagem;
+  textoDescricao.textContent = dados[index].texto;
+
+  // Atualiza índice atual
+  i = index;
+}
+
+// Auto-play
+function iniciarLoop() {
+  intervalo = setInterval(() => {
+    let proximo = (i + 1) % categoriasVantagens.length;
+    atualizarVantagem(proximo);
+  }, 5000);
+}
+
+// Eventos de clique
+for (let j = 0; j < categoriasVantagens.length; j++) {
+  categoriasVantagens[j].addEventListener("click", () => {
+    clearInterval(intervalo); // para autoplay
+    atualizarVantagem(j);
+    setTimeout(iniciarLoop, 5000); // volta a rodar depois de 5s
+  });
+}
+
+// Inicializa
+atualizarVantagem(0);
+iniciarLoop();
+
